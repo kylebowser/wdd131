c51b8e0163bf64492fe4f91ed6f8d908d1f316bc
kylebowser <kylebowser9@gmail.com>
2025-11-08 11:43:38 -0700

    Week 2 activities

--- week2/scripts/bom-scripts.js
+++ week2/scripts/bom-scripts.js
@@ -1,15 +1,28 @@
-const input = document.querySelector('#favechap');
+const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
-const li = document.createElement('li');
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
 
-const deleteButton = document.createElement('button');
+        const deleteButton = document.createElement('button');
 
-li.textContent = input.value;
+        li.textContent = input.value;
 
-deleteButton.textContent = 'X';
+        deleteButton.textContent = 'X';
 
-li.append(deleteButton);
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
 
-list.append(li);
+        });
+
+        li.append(deleteButton);
+        list.append(li);
+
+        input.value = '';
+        input.focus();
+    }
+});
+
